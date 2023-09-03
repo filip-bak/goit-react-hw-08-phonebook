da0df0157d012239e36349e51ed3cbadd0f57e49
Filip Bączkowski <filo.b@wp.pl>
2023-09-03 04:43:48 +0200

    refactor: section component

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,13 +1,25 @@
+import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
   return (
     <>
-      <h2>{title}</h2>
+      <Typography
+        sx={{ fontSize: ['2rem', '2.5rem'] }}
+        variant="h3"
+        fontWeight={500}
+        textTransform="uppercase"
+        color="primary.main"
+      >
+        {title}
+      </Typography>
       {children}
     </>
   );
 };
-Section.propTypes = { title: PropTypes.string };
+Section.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
 
 export default Section;
