6b70a64a0235f8a8a3c25431616899298e7ff598
Filip Bączkowski <filo.b@wp.pl>
2023-09-03 04:37:02 +0200

    feat: add restricted and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,51 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { fetchContacts } from 'redux/contacts/actions';
 import { Route, Routes } from 'react-router-dom';
 
-import PageNotFound from 'Pages/PageNotFound';
-import Home from 'Pages/Home';
-import SharedLayout from './components/SharedLayout';
-import RegisterPage from 'Pages/RegisterPage';
-import LoginPage from 'Pages/LoginPage';
 import ContactsPage from 'Pages/ContactsPage';
+import Home from 'Pages/Home';
+import LoginPage from 'Pages/LoginPage';
+import PageNotFound from 'Pages/PageNotFound';
+import RegisterPage from 'Pages/RegisterPage';
+import { Spinner } from 'components/Spinner';
+import { useAuthUser } from 'hooks/useAuthUser';
+import PrivateRoute from 'layoutRoutes/PrivateRoute';
+import { RestrictRoute } from 'layoutRoutes/RestrictRoute';
 import { refreshUser } from 'redux/auth/actions';
+import SharedLayout from './components/SharedLayout';
 
 export const App = () => {
   const dispatch = useDispatch();
+
+  const { isRefreshing } = useAuthUser();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
+  if (isRefreshing)
+    return <Spinner loading={isRefreshing} left={'50%'} top={'50%'} />;
+
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RestrictRoute component={<RegisterPage />} redirectTo="/" />
+          }
+        />
+        <Route
+          path="/login"
+          element={<RestrictRoute component={<LoginPage />} redirectTo="/" />}
+        />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
